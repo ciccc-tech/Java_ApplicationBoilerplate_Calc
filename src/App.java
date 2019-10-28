bcfca6dabc85f6eafa1fc8432208f6956fa099fc
Ivan Malone <ivanincanada@gmail.com>
2019-10-28 04:21:28 -0700

    added CommandLine.java & some code to App.Java

--- src/App.java
+++ src/App.java
@@ -7,12 +7,27 @@
  *
  */
 public class App {
+	
+	
+	// A Reference to our CommandLine object we created in CommandLine.java
+	private static CommandLine cmdLine;
+	
+	// Set a default value to false will keep our program running
+	private static boolean _shouldTerminate = false;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// create an 'instance' of CommandLine object with the name cmdLine
+		cmdLine = new CommandLine(args); // give it 'args' so that it knows what arguments were used (if any) on startup
+		
+		do  // this code will be run at least 'once' and if _shouldTerminate == false, then it will continue to loop
+			{
+			
+			} while (_shouldTerminate == false); // will only exit if _shouldTerminate is true 
 
 	}
 
