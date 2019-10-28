0ba2c9e64246701f2cf5004c0e84330c2aa9d842
Ivan Malone <ivanincanada@gmail.com>
2019-10-28 04:25:40 -0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -9,8 +9,6 @@
 public class App {
 	
 	
-	// A Reference to our CommandLine object we created in CommandLine.java
-	private static CommandLine cmdLine;
 	
 	// Set a default value to false will keep our program running
 	private static boolean _shouldTerminate = false;
@@ -21,8 +19,6 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// create an 'instance' of CommandLine object with the name cmdLine
-		cmdLine = new CommandLine(args); // give it 'args' so that it knows what arguments were used (if any) on startup
 		
 		do  // this code will be run at least 'once' and if _shouldTerminate == false, then it will continue to loop
 			{
