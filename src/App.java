d5f84186a63bd56825edc62a48e9ca6cb8253bdc
Ivan Malone <ivanincanada@gmail.com>
2019-10-28 06:05:41 -0700

    ...

--- src/App.java
+++ src/App.java
@@ -16,21 +16,23 @@ public class App {
 	private static boolean _shouldTerminate = false;
 	
 	// If true, we will run our tests
-	private static boolean _shouldRunTests = true;
+	private static boolean _shouldRunTests = false;
 
 	/**
 	 * @param args
-	 */
+	 */	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		// create an 'instance' of CommandLine object with the name cmdLine
 		cmdLine = new CommandLine(args); // give it 'args' so that it knows what arguments were used (if any) on startup
 		
-		
+		// set this depending on if -runTests is on the command line or not
+		_shouldRunTests = cmdLine.doesArgumentExists("-runTests");		
 		// should we run our tests?
 		if (_shouldRunTests == true)
 			{
+			Debug.log("Found -runTests in argument list: - will run tests now");
 			//lets run some tests
 			Test.Test_Console_RunAll();
 			}
--- src/CommandLine.java
+++ src/CommandLine.java
@@ -8,13 +8,42 @@
  */
 public class CommandLine {
 	
+	
+	private String[] arguments = new String[0];
+	
 	public CommandLine(String[] arguments)
 	{// the constructor will be called with whatever arguments (if any) were known from the 'main' method
 		
-		// just print a test message for now
+	// initialize our internal argument array to the same size as the one passed to the constructor
+	this.arguments = new String[arguments.length];
 	
+	// begin a loop and start copying arguments to this.arguments, one element at a time
+	for (int i = 0; i < arguments.length; i++)
+		{
+		this.arguments[i] = arguments[i];
+		}
+	// now we have saved a copy of the arguments
+	
+	}	
+	public boolean doesArgumentExists(String argument)
+		{
+		for (int i = 0; i < this.arguments.length; i ++)
+			{
+			if (this.arguments[i].contains(argument))
+				{				
+				return true;
+				}
+			}
+		return false;
+		}
+	
+	public int getArgumentIndex(String argument)
+		{
+		for (int i = 0; i < this.arguments.length; i ++)
+			{
+			if (this.arguments[i].contains(argument))
+				{return i;}
+		}
+		return -1;
+		}		
 	}
-	
-	
-
-}
